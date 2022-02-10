f9ae7f596791c5995b373b2e609afe5da57bc18d
ZenxMato <97756244+ZenxMato@users.noreply.github.com>
2022-02-10 11:16:46 +0700

    Update logomaker.py

--- PrimeMega/modules/logomaker.py
+++ PrimeMega/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ᴘʀɪᴍᴇ ᴍᴇɢᴀ](https://t.me/PrimeMegaBot)",
+            caption="Logo by [Swain Robot](https://t.me/Swainrobot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ᴘʀɪᴍᴇ ᴍᴇɢᴀ](https://t.me/PrimeMegaBot)",
+            caption="Logo by [Swain Robot](https://t.me/swainrobot)",
             force_document=False,
         )
         os.remove(flnme)
